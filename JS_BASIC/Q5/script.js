80f12c3fa140ca64b506e2732b67ba41e5193795
intenseG <intenseg1515@gmail.com>
2021-06-15 12:33:18 +0900

    5-1は消費税を算出するよう修正

--- JS_BASIC/Q5/script.js
+++ JS_BASIC/Q5/script.js
@@ -2,8 +2,8 @@
 var banana = {name: "バナナ", price: 200};
 var orange = {name: "オレンジ", price: 100};
 
-console.log(banana['price']);
-console.log(orange['price']);
+console.log(banana['price'] * 0.1);
+console.log(orange['price'] * 0.1);
 
 // 5-2
 console.log(banana['price'] + orange['price']);
